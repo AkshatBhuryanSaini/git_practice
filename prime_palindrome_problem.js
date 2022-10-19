beaddfddb0b36da8c8c9e771ad2e04aba51649f6
Akshat Bhuryan <me@here.com>
2022-10-19 15:01:53 +0530

    changes occur in prime and palindrome problem

--- prime_palindrome_problem.js
+++ prime_palindrome_problem.js
@@ -1,9 +1,38 @@
+
+/////--------Check_Prime------
+
+
 var num=13;
 var count=0;
 
 for(var i=1; i<=num; i++){
-if(num%i==0){
-count++;
+	if(num%i==0){
+	count++;
+	}
 }
+
+if(count==2){
+console.log("Yes");
+}
+else{
+console.log("No");
+}
+
+
+/////----------Palindrome---------
+
+
+var str="naman";
+var bag="";
+
+for(var i=str.length-1; i>=0; i--){
+	bag+=str[i];
+}
+
+if(bag==str){
+console.log("Yes");
+}
+else{
+console.log("No");
 }
 
